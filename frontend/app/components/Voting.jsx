4105df1625385141f33c6f67c097e643bc193728
Beubeu <nguyen.hl.b@gmail.com>
2024-03-12 20:00:33 +0100

    improvement: remove unnecessary comments

--- frontend/app/components/Voting.jsx
+++ frontend/app/components/Voting.jsx
@@ -19,26 +19,6 @@ import { parseAbiItem } from 'viem'
 
 // import { publicClient } from '../network/client'
 
-// l’enregistrement d’une liste blanche d'électeurs.
-
-// à l'administrateur de commencer la session d'enregistrement de la proposition.
-
-// aux électeurs inscrits d’enregistrer leurs propositions.
-
-// à l'administrateur de mettre fin à la session d'enregistrement des propositions.
-
-// à l'administrateur de commencer la session de vote.
-
-// aux électeurs inscrits de voter pour leurs propositions préférées.
-
-// à l'administrateur de mettre fin à la session de vote.
-
-// à l'administrateur de comptabiliser les votes.
-
-// à tout le monde de consulter le résultat.
-
-// Lister les événements.
-
 const Voting = () => {
 
     const { address, isConnected } = useAccount();
