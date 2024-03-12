697e286d1a67ce497e8eda4a1bec6a10f95d9148
Beubeu <nguyen.hl.b@gmail.com>
2024-03-12 13:08:31 +0100

    fix: indentation in voting.jsx

--- frontend/app/components/Voting.jsx
+++ frontend/app/components/Voting.jsx
@@ -5,10 +5,10 @@ import { useEffect, useState } from 'react'
 import { Box, Flex, Text, Input, Button, useToast, Heading, Spinner } from '@chakra-ui/react'
 
 import {
-  Alert,
-  AlertIcon,
-  AlertTitle,
-  AlertDescription,
+    Alert,
+    AlertIcon,
+    AlertTitle,
+    AlertDescription,
 } from '@chakra-ui/react'
 
 import { contractAddress, contractAbi, workflowStatuses } from '@/constants'
@@ -41,99 +41,94 @@ import { parseAbiItem } from 'viem'
 
 const Voting = () => {
 
-  const { address, isConnected } = useAccount();
+    const { address, isConnected } = useAccount();
 
-  const toast = useToast();
+    const toast = useToast();
 
-  const [voterAddress, setVoterAddress] = useState('');
+    const [voterAddress, setVoterAddress] = useState('');
 
-  // Is owner
-  function isOwner() {
+    // Is owner
+    function isOwner() {
     if (!isConnected) {
-      return false
+        return false
     } else {
-      const { data: ownerAddress } = useContractRead({
+        const { data: ownerAddress } = useContractRead({
         address: contractAddress,
         abi: contractAbi,
         functionName: 'owner',
-      })
-      return ownerAddress === address;
+        })
+        return ownerAddress === address;
     }
-  }
+    }
 
-  // Get voter data (not tested yet)
-  const { data: getVoterData, error: getVoterError, isPending: getVoterIsPending, refetch } = useReadContract({
-    address: contractAddress,
-    abi: contractAbi,
-    functionName: 'getVoter',
-    account: address,
-    args: [voterAddress]
-  })
-
-  // Get workflow status
-  const { data: getWorkflowStatusData, error: getWorkflowStatusError, isPending: getWorkflowStatusIsPending } = useReadContract({
-    address: contractAddress,
-    abi: contractAbi,
-    functionName: 'workflowStatus',
-    account: address
-  })
-
-  // Add voter
-  const { data: hash, error, isPending: addVoterIsPending, writeContract: addVoterCall } = useWriteContract({
-    mutation: {
-        onSuccess: () => {
-            toast({
-              title: "Le voter a bien été ajouté",
-              status: "success",
-              duration: 3000,
-              isClosable: true,
-            });
-        },
-        onError: (error) => {
-            toast({
-              title: error.message,
-              status: "error",
-              duration: 3000,
-              isClosable: true,
-            });
-        },
-    },
-  });
-
-
-  // Add voter
-  const addVoter = async() => {
-    addVoterCall({
+    // Get voter data (not tested yet)
+    const { data: getVoterData, error: getVoterError, isPending: getVoterIsPending, refetch } = useReadContract({
         address: contractAddress,
         abi: contractAbi,
-        functionName: 'addVoter',
-        args: [voterAddress],
+        functionName: 'getVoter',
+        account: address,
+        args: [voterAddress]
     })
-  }
 
-  useEffect (() => {
-    console.log('hash', hash)
-  }, [hash])
+    // Get workflow status
+    const { data: getWorkflowStatusData, error: getWorkflowStatusError, isPending: getWorkflowStatusIsPending } = useReadContract({
+        address: contractAddress,
+        abi: contractAbi,
+        functionName: 'workflowStatus',
+        account: address
+    })
+
+    // Add voter
+    const { data: hash, error, isPending: addVoterIsPending, writeContract: addVoterCall } = useWriteContract({
+        mutation: {
+            onSuccess: () => {
+                toast({
+                    title: "Le voter a bien été ajouté",
+                    status: "success",
+                    duration: 3000,
+                    isClosable: true,
+                });
+            },
+            onError: (error) => {
+                toast({
+                    title: error.message,
+                    status: "error",
+                    duration: 3000,
+                    isClosable: true,
+                });
+            },
+        },
+    });
 
 
-  return (
+  // Add voter
+    const addVoter = async() => {
+        addVoterCall({
+            address: contractAddress,
+            abi: contractAbi,
+            functionName: 'addVoter',
+            args: [voterAddress],
+        })
+    }
+
+    return (
     <Box
-      direction="column"
-      width="100%"
+        direction="column"
+        width="100%"
     >
-      <Flex width="100%">
+        <Flex width="100%">
         {getWorkflowStatusIsPending ? (
-                  <Spinner />
-              ) : (
-                  <Text><b>Workflow status :</b> {workflowStatuses[getWorkflowStatusData]}</Text>
-          )}
-      </Flex>
-      <Flex>
+                    <Spinner />
+                ) : (
+                    <Text><b>Workflow status :</b> {workflowStatuses[getWorkflowStatusData]}</Text>
+            )}
+        </Flex>
+        <Flex>
             <Input placeholder='New voter address' onChange={(e) => setVoterAddress(e.target.value)} />
             <Button disabled={addVoterIsPending} onClick={addVoter}>{addVoterIsPending ? 'Confirming...' : 'Add voter'} </Button>
-      </Flex>
+        </Flex>
     </Box>
-  )
-}
+    )
+    }
 
 export default Voting
