d51ce6617dc64cc1e8a3ac01be3ba80abf508553
medhi.khoule <khoule.medhi@hotmail.fr>
2024-03-17 18:06:58 +0100

    Update cleaning code

--- frontend/app/components/Notconnected.jsx
+++ frontend/app/components/Notconnected.jsx
@@ -12,18 +12,18 @@ import WinningProposal from './WinningProposal';
 const NotConnected = (getWorkflowStatus, address) => {
     return (
         <>
-        <Flex direction="column" justifyContent="center" width="100%">
-            <Card align='center'>
-                <CardHeader>
-                    <Heading size='md'>
-                        <Alert status='warning'>
-                            <AlertIcon />
-                            Please connect your Wallet.
-                        </Alert>
-                    </Heading>
-                    <WinningProposal workflowStatus={getWorkflowStatus} address={address} />
-                </CardHeader>
-            </Card>
+            <Flex direction="column" justifyContent="center" width="100%">
+                <Card align='center'>
+                    <CardHeader>
+                        <Heading size='md'>
+                            <Alert status='warning'>
+                                <AlertIcon />
+                                Please connect your Wallet.
+                            </Alert>
+                        </Heading>
+                        <WinningProposal workflowStatus={getWorkflowStatus} address={address} />
+                    </CardHeader>
+                </Card>
             </Flex>
         </>
     )
