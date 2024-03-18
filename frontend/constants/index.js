a69781d8d2bfdb11ef0dde896e9448926110e43e
Beubeu <nguyen.hl.b@gmail.com>
2024-03-18 10:12:50 +0100

    improvement: add contract address in .env

--- frontend/constants/index.js
+++ frontend/constants/index.js
@@ -1,4 +1,4 @@
-export const contractAddress="0x5FbDB2315678afecb367f032d93F642f64180aa3"
+export const contractAddress=process.env.NEXT_PUBLIC_CONTRACT_ADDRESS;
 export const contractAbi=[
   {
     "inputs": [],
